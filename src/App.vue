b708fc4adf21fd1568bbb2f27c8e3b9867803e96
J Daniel Soto C <joseda.sotoca@gmail.com>
2025-04-07 11:06:40 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="fondo">
+    <router-view />
+    <!-- <ToastContainer /> -->
+  </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+</script>
+
+<style scoped>
+.fondo {
+  font-family: 'Luckiest Guy', cursive;
+  background: url("./images/fondo.jpg") no-repeat center center fixed;
+  background-size: cover;
+  min-height: 100vh;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import { getAuth } from 'firebase/auth';
+
+// Importar componentes
+import Main from '../pages/Main.vue';
+import Home from '../pages/Home.vue';
+import Login from '../pages/Login.vue';
+import Register from '../pages/Register.vue';
+
+
+const routes = [
+  { path: '/', name: 'main', component: Main },
+  { path: '/login', name: 'login', component: Login },
+  { path: '/register', name: 'register', component: Register },
+  { path: '/inicio', name: 'inicio', component: Home, meta: { requiresAuth: true } },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const auth = getAuth();
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  const user = auth.currentUser; 
+
+  if (requiresAuth && !user) {
+    console.log("Acceso denegado. Redirigiendo a /login");
+    next('/login');
+  } else {
+    next();
+  }
+});
+
+export default router;
